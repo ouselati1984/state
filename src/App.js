33a8ba2e0229eba08be6b849dd23eb958e83ed87
Oueslati_Ahmed <oueslati.ahmed84@gmail.com>
2021-08-03 22:37:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+
+
+class App extends React.Component {
+  
+  state = {
+
+    Person: {
+
+      fullName: "Oueslati Ahmed",
+
+      bio: "I just love the life",
+
+      imgSrc: "Ahmed.jpg",
+
+      profession: "I am a Front Office & Back Office Administrator"
+
+    },
+    
+
+    show: true
+
+  };
+
+ 
+
+  handleShowPerson = () => {
+
+    this.setState({
+
+      ...this.state,
+
+      show: !this.state.show
+
+    });
+
+  };
+
+
+
+  render() {
+
+    return (
+
+      <>
+
+        {this.state.show && (
+
+          <>
+
+            <h1>{this.state.Person.fullName}</h1>
+
+            <h1>{this.state.Person.bio}</h1>
+
+            <img src={this.state.Person.imgSrc} alt="Moi"></img>
+
+            <h1>{this.state.Person.profession}</h1>
+           
+            
+            <br></br>
+
+          </>
+
+        )}
+
+ 
+
+        <button onClick={this.handleShowPerson}>click here</button>
+
+      </>
+
+    );
+
+  }
+
 }
 
 export default App;
